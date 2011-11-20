fdbb46dc2f0e83532f909ad9e307b08bb9b7f4b1
rwl <r.w.lincoln@gmail.com>
2011-11-20 04:01:51 +0000

    Adding muntjac package.

---
+++ traitsui/muntjac/__init__.py
@@ -0,0 +1,32 @@
+#------------------------------------------------------------------------------
+# Copyright (C) 2007 Riverbank Computing Limited
+# Copyright (C) 2011 Richard Lincoln
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#------------------------------------------------------------------------------
+
+""" Defines the concrete implementations of the traits Toolkit interface for
+    the Muntjac user interface toolkit.
+"""
+
+#------------------------------------------------------------------------------
+#  Define the reference to the exported GUIToolkit object:
+#------------------------------------------------------------------------------
+
+import toolkit
+
+# Reference to the GUIToolkit object for PyQt.
+toolkit = toolkit.GUIToolkit()
+
+# EOF -------------------------------------------------------------------------
---
+++ traitsui/muntjac/constants.py
@@ -0,0 +1,57 @@
+#------------------------------------------------------------------------------
+# Copyright (C) 2007 Riverbank Computing Limited
+# Copyright (C) 2011 Richard Lincoln
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#------------------------------------------------------------------------------
+
+""" Defines constants used by the Muntjac implementation of the various text
+    editors and text editor factories.
+"""
+
+#------------------------------------------------------------------------------
+#  Imports:
+#------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------
+#  Constants:
+#------------------------------------------------------------------------------
+
+# Default dialog title
+DefaultTitle = 'Edit properties'
+
+# Color of valid input
+OKColor = ''
+
+# Color to highlight input errors
+ErrorColor = ''
+
+# Color for background of read-only fields
+ReadonlyColor = ''
+
+# Color for background of fields where objects can be dropped
+DropColor = ''
+
+# Color for an editable field
+EditableColor = ''
+
+# Color for background of windows (like dialog background color)
+WindowColor = ''
+
+# Screen size values:
+screen_dx = 0
+screen_dy = 0
+
+# EOF -------------------------------------------------------------------------
